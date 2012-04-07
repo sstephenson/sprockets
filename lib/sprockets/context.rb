f0b36353e581c532d637d4a6cc0afd69999728c4
Dmitriy Vorotilin <mydeeptown@gmail.com>
2012-04-07 23:44:57 +0400

    Fix Context#resolve for absolute paths

--- lib/sprockets/context.rb
+++ lib/sprockets/context.rb
@@ -77,32 +77,34 @@ module Sprockets
     #     # => "/path/to/app/javascripts/bar.js"
     #
     def resolve(path, options = {}, &block)
-      pathname   = Pathname.new(path)
-      attributes = environment.attributes_for(pathname)
+      attributes = environment.attributes_for(path)
+      pathname = attributes.pathname
 
       if pathname.absolute?
-        pathname
-
-      elsif content_type = options[:content_type]
-        content_type = self.content_type if content_type == :self
-
-        if attributes.format_extension
-          if content_type != attributes.content_type
-            raise ContentTypeMismatch, "#{path} is " +
-              "'#{attributes.content_type}', not '#{content_type}'"
-          end
-        end
-
-        resolve(path) do |candidate|
-          if self.content_type == environment.content_type_of(candidate)
-            return candidate
-          end
-        end
-
-        raise FileNotFound, "couldn't find file '#{path}'"
+        return pathname if pathname.exist?
       else
-        environment.resolve(path, {:base_path => self.pathname.dirname}.merge(options), &block)
+        if content_type = options[:content_type]
+          content_type = self.content_type if content_type == :self
+
+          if attributes.format_extension
+            if content_type != attributes.content_type
+              raise ContentTypeMismatch, "#{path} is " +
+                "'#{attributes.content_type}', not '#{content_type}'"
+            end
+          end
+
+          resolve(path) do |candidate|
+            if self.content_type == environment.content_type_of(candidate)
+              return candidate
+            end
+          end
+
+        else
+          return environment.resolve(path, {:base_path => self.pathname.dirname}.merge(options), &block)
+        end
       end
+
+      raise FileNotFound, "couldn't find file '#{path}'"
     end
 
     # `depend_on` allows you to state a dependency on a file without
