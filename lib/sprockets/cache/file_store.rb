1966ab695a8a947821e251da3796e1a570d78b8a
Rubem Nakamura <rubem.nakamura@gmail.com>
2014-03-20 16:20:22 -0300

    add file store atomic write

--- lib/sprockets/cache/file_store.rb
+++ lib/sprockets/cache/file_store.rb
@@ -41,7 +41,9 @@ module Sprockets
         exists = File.exist?(path)
 
         # Write data
-        File.open(path, 'w') { |f| Marshal.dump(value, f) }
+        atomic_write(path) do |file|
+          file.write(Marshal.dump(value, file))
+        end
 
         # GC if necessary
         @size += 1 unless exists
@@ -51,6 +53,53 @@ module Sprockets
       end
 
       private
+        # extracted from https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/file/atomic.rb
+        def atomic_write(file_name, temp_dir = Dir.tmpdir)
+          require 'tempfile' unless defined?(Tempfile)
+          require 'fileutils' unless defined?(FileUtils)
+
+          temp_file = Tempfile.new(File.basename(file_name), temp_dir)
+          temp_file.binmode
+          yield temp_file
+          temp_file.close
+
+          if File.exist?(file_name)
+            # Get original file permissions
+            old_stat = File.stat(file_name)
+          else
+            # If not possible, probe which are the default permissions in the
+            # destination directory.
+            old_stat = probe_stat_in(File.dirname(file_name))
+          end
+
+          # Overwrite original file with temp file
+          FileUtils.mv(temp_file.path, file_name)
+
+          # Set correct permissions on new file
+          begin
+            File.chown(old_stat.uid, old_stat.gid, file_name)
+            # This operation will affect filesystem ACL's
+            File.chmod(old_stat.mode, file_name)
+          rescue Errno::EPERM
+            # Changing file ownership failed, moving on.
+          end
+        end
+
+        def probe_stat_in(dir)
+          basename = [
+            '.permissions_check',
+            Thread.current.object_id,
+            Process.pid,
+            rand(1000000)
+          ].join('.')
+
+          file_name = File.join(dir, basename)
+          FileUtils.touch(file_name)
+          File.stat(file_name)
+        ensure
+          FileUtils.rm_f(file_name) if file_name
+        end
+
         def find_caches
           Dir.glob(File.join(@root, '**/*.cache'))
         end
